a9f92db0b24170ed7f9c24995894b96e2acebeb7
Ethan Hansen <hanseneg@hotmail.com>
2020-06-22 16:15:15 -0600

    commit 6

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ box.addEventListener("mouseover", mouseOver);
 function mouseOver() {
     box.style.color = "blue"
 }
+
 box.addEventListener("mousedown", mouseDown);
 function mouseDown() {
     box.style.color = "red"
@@ -14,4 +15,12 @@ function mouseUp() {
 box.addEventListener("dblclick", dblClick);
 function dblClick() {
     box.style.color = "green"
+}
+box.addEventListener("scroll", scroll1);
+function scroll1() {
+    box.style.color = "orange"
+}
+box.addEventListener("mouseout", mouseOut);
+function mouseOut() {
+    box.style.color = "black"
 }
